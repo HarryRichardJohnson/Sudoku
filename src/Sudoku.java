fd0540c2e7f7f66b55a39e733138a5502cd552d4
Harry Richard Johnson <harry.richard.johnson@gmail.com>
2020-12-19 21:51:01 +1300

    Loops 81 times
    loops 81 times creating 81 random numbers. Prints this tothe console.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,11 +3,22 @@ public class Sudoku {
     public static void main(String[] args) {
 
         int[][] sudokuBoard = new int[9][9];
-        //declare a 9 x 9 array to store our sudoku board
+        //Declare a 9 x 9 array to store our sudoku board
 
         for (int i = 0; i < 9; i++) {
-            int randomNum = 1 + (int) (9 * Math.random());
-            System.out.println(randomNum);
+            //Loops 9 times, there are nine rows in a board
+            for (int j = 0; j < 9; j++) {
+            //Loops 9 times, there are 9 columns in a board
+                int randomNum = 1 + (int) (9 * Math.random());
+                //Generates a random number for 1 to 9
+                System.out.print("i =" + i + " ");
+                //Prints value of i
+                System.out.print("j =" + j + " ");
+                //Prints value of j
+                System.out.println("Random number = " + randomNum);
+                //Prints the random number generated above
+
+            }
         }
     }
 }
